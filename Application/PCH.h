4e3285261189e154b64100ef5d848e535967b196
NanSu <nansu0425@gmail.com>
2026-04-05 05:54:27 +0900

    feat(debug): add BA_ASSERT macro for debug builds

--- Application/PCH.h
+++ Application/PCH.h
@@ -1,4 +1,21 @@
-#pragma once
+﻿#pragma once
 
 #include <Windows.h>
 #include <cstdio>
+
+#ifdef _DEBUG
+#define BA_ASSERT(expression)                                                                                                  \
+    do                                                                                                                         \
+    {                                                                                                                          \
+        if (!(expression))                                                                                                     \
+        {                                                                                                                      \
+            CHAR buffer[512] = {};                                                                                             \
+            sprintf_s(buffer, "Assertion failed!\n\nExpression: %s\nFile: %s\nLine: %d\n\n", #expression, __FILE__, __LINE__); \
+            OutputDebugStringA(buffer);                                                                                        \
+            __debugbreak();                                                                                                    \
+        }                                                                                                                      \
+    }                                                                                                                          \
+    while (0)
+#else
+#define BA_ASSERT(expression)
+#endif
